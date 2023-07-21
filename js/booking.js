38a0918c46c8390df81268caa73fcf3a5f09f815
Xushmuratov Ibrohim <xushmurodovibrohim3@gmail.com>
2023-07-21 23:11:20 +0500

    edit

--- js/booking.js
+++ js/booking.js
@@ -8,7 +8,7 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
    years = [2023 , 2024, 2022],
    days = ["01", "02", "03", "04", "05", "06", "07", '08', "09", 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
    hours = ["01", "02", "03", "04", "05", "06", "07", '08', "09", 10, 11, 12],
-   minutes = ["01", "02", "03", "04", "05", "06", "07", '08', "09", 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+   minutes = ['00' , "01", "02", "03", "04", "05", "06", "07", '08', "09", 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
 
 const selectMonth = document.querySelector('#month'),
    selectYear = document.querySelector('#year'),
@@ -54,7 +54,7 @@ fillingSelects(selectMonth, months , "index")
 fillingSelects(selectYear, years , "number")
 fillingSelects(selectDay , days , 'index')
 fillingSelects(selectHour, hours , "index")
-fillingSelects(selectMinut, minutes, "index")
+fillingSelects(selectMinut, minutes, "number")
 
 selectMonth.addEventListener('change', changeDaySelect)
 
@@ -67,7 +67,6 @@ form.addEventListener('submit', (event) => {
       return "AAA"
    } 
    const inputs = form.querySelectorAll('input')
-   console.log(peopleCount);
    let userData = {tableTimeData};
    userData.peopleCount = localStorage.getItem("peopleCount")
    inputs.forEach(e => {
@@ -150,6 +149,7 @@ function changeDaySelect() {
 function validateDate() {
    let hour = selectAmOrPm.value === 'pm' ? +selectHour.value + 12 : selectHour.value
    tableTimeData = new Date(addZero(selectYear.value) + "-" + addZero(selectMonth.value) + '-' + addZero(selectDay.value) + "T" + addZero(hour) + ":" + addZero(selectMinut.value) + ':' + "00" + "Z")
+   // console.log(addZero(selectYear.value) + "-" + addZero(selectMonth.value) + '-' + addZero(selectDay.value) + "T" + addZero(hour) + ":" + addZero(selectMinut.value.innerText) + ':' + "00" + "Z");
    let now = new Date()
    let limitDate = new Date()
    limitDate.setMonth(limitDate.getMonth() + 3)
